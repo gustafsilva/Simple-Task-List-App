8816f19f0fcd9b575699de1c61333ec7048a798b
Gustavo F. Silva <gustafsilva@hotmail.com>
2018-08-27 19:16:44 -0300

    Creating component for main screen (single), without connection to database. The to-do list is being stored in working memory

--- App.js
+++ App.js
@@ -1,10 +1,78 @@
 import React, { Component } from 'react'
-import { Container } from 'native-base';
+import { Keyboard, ScrollView } from 'react-native';
+import { Container, Content, Form, Item, Input, Button, Text } from 'native-base';
 
+import SimpleHeader from './src/header';
+import Task from './src/task';
+
+/** Component App, responsible for rendering the single screen.
+ * You will be responsible for showing the to-do lists such as adding new tasks.
+ * Component Task helps in rendering the task list.
+ */
 export default class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      tasks: [],
+      taskTextInput: ""
+    }
+
+    this.addTask = this.addTask.bind(this);
+  }
+
+  addTask() {
+    if (this.state.taskTextInput !== "") {
+      /* If a task whith empty text has not been passed */
+      Keyboard.dismiss();
+
+      let newState = this.state;
+      let newTask = {
+        completed: false,
+        text: this.state.taskTextInput // captures the passed text
+      }
+
+      newState.tasks.push(newTask);
+      newState.taskTextInput = ""; // reset input
+
+      this.setState(newState);
+    }
+  }
+
+  /* Funcion responsible for rendering task list */
+  _renderTaskList() {
+    let tasksView = this.state.tasks.map((task, key) => {
+      return (<Task text={task.text} completed={task.completed} key={key} />)
+    });
+
+    return tasksView;
+  }
+
   render() {
     return (
       <Container>
+        <SimpleHeader />
+
+        <Content>
+          <Form>
+            <Item regular>
+              <Input
+                placeholder="Nova Tarefa"
+                onChangeText={(taskTextInput) => this.setState({ taskTextInput: taskTextInput })}
+                value={this.state.taskTextInput}
+              />
+            </Item>
+            <Button onPress={this.addTask} success full>
+              <Text>Adicionar</Text>
+            </Button>
+          </Form>
+
+          <ScrollView>
+            {
+              this._renderTaskList()
+            }
+          </ScrollView>
+        </Content>
       </Container>
     );
   }
